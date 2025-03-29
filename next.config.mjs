d44456725b5eb2a2199e87e0ba379516bc0fb923
Chirantan Patra <139316921+cpatra11@users.noreply.github.com>
2025-03-29 23:24:25 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["pdf-parse"],
+    outputFileTracingIncludes: {
+      "/*": ["./cache/**/*"],
+      "/api/**/*": ["node_modules/tiktoken/tiktoken_bg.wasm"],
+    },
+    outputFileTracingExcludes: {
+      "/api/files/*": [
+        ".next/**/*",
+        "node_modules/**/*",
+        "public/**/*",
+        "app/**/*",
+      ],
+    },
+  },
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
+};
+
+export default nextConfig;
